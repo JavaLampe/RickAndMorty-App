bfa0fe669cf97c6e1bcc8b03f7e43e12395bf2dc
Matthias <details@gmx.de>
2023-05-11 15:58:35 +0200

    button fix

--- components/nav-button/nav-button.js
+++ components/nav-button/nav-button.js
@@ -5,5 +5,7 @@ export function navButton() {
     buttonNext.classList.add("button", "button--next");
     buttonPrev.setAttribute("data-js", "button-prev");
     buttonNext.setAttribute("data-js", "button-next");
+    buttonPrev.setAttribute("onclick", "prevPage()");
+    buttonNext.setAttribute("onclick", "nextPage()");
     return [buttonPrev, buttonNext]
 }
