809d99581839ece35ad02775c863d03f70c58ba3
Matthias <details@gmx.de>
2023-05-11 15:42:51 +0200

    nav button component added

--- components/nav-pagination/nav-pagination.js
+++ components/nav-pagination/nav-pagination.js
@@ -1,16 +1,14 @@
+import {navButton} from "../nav-button/nav-button.js"
+
 export function createPagination() {
     const nav = document.createElement("nav");
-    const buttonPrev = document.createElement("button");
-    const buttonNext = document.createElement("button");
-    const span = document.createElement("span") 
+    const span = document.createElement("span");
+    const button = navButton(); 
     nav.classList.add("navigation");
     nav.setAttribute("data-js", "navigation");
-    buttonPrev.classList.add("button", "button--prev");
-    buttonNext.classList.add("button", "button--next");
-    buttonPrev.setAttribute("data-js", "button-prev");
-    buttonNext.setAttribute("data-js", "button-next");
     span.classList.add("navigation__pagination");
     span.setAttribute("data-js", "pagination");
-    nav.append(buttonPrev, span, buttonNext);
+    nav.append(button[0], span, button[1]);
+    return nav
 }
 
