c0e4eb91c3be73e46fc9fd3c17f5da5667504e84
thomas jubin <thomas.jubin92@gmail.com>
2023-05-11 10:25:22 +0200

    data sind da

--- index.js
+++ index.js
@@ -12,3 +12,14 @@ const pagination = document.querySelector('[data-js="pagination"]');
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
+
+async function fetchCharacters() {
+  try {
+    const response = await fetch("https://rickandmortyapi.com/api/character");
+    const data = await response.json();
+    if (!response) {
+      console.log("error: ", response.status);
+    }
+    cardContainer.innerHTML = "";
+  } catch (error) {}
+}
